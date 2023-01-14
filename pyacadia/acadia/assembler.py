a6c8c2dcb2dddfabbda375844619e56628c505b6
Billy Kalfus <william.kalfus@yale.edu>
2023-01-14 18:02:54 -0500

    Nightly WIP push, added Processor mixins, better PEP8 compliance

--- pyacadia/acadia/assembler.py
+++ pyacadia/acadia/assembler.py
@@ -1,20 +1,30 @@
 """
-assembler.py
 Assembler for the microarchitecture of the Acadia quantum control system.
-William Kalfus, Yale University
-December 2022
 """
+
+__all__ = ["Operable", 
+           "Operation", 
+           "Symbol", 
+           "ManagedResource"]
+
 from types import MethodType
 from abc import ABC
-import uuid
 from operator import and_
 
 class Operable(type):
-    OPERATORS = ["eq", "ne", "gt", "lt", "ge", "le", "neg", "abs", "invert", "add", "radd", "sub", "rsub", "mul", "rmul", "floordiv", "rfloordiv", "truediv", "rtruediv", "mod", "rmod", "pow", "rpow", "lshift", "rlshift", "rshift", "rrshift", "and", "rand", "or", "ror", "xor", "rxor", "bool", "len", "contains", "iter", "getitem", "setitem", "getattr", "setattr", "call", "enter", "exit", "copy", "deepcopy"]
-    
     """
-    A metaclass used to capture operators acting on instances of derived classes and return a symbolic representation of the operator call.
-    """ 
+    A metaclass used to capture operators acting on instances of derived 
+    classes and return a symbolic representation of the operator call.
+    """
+    
+    OPERATORS = ["eq", "ne", "gt", "lt", "ge", "le", "neg", "abs", "invert",
+                 "add", "radd", "sub", "rsub", "mul", "rmul", "floordiv", 
+                 "rfloordiv", "truediv", "rtruediv", "mod", "rmod", "pow", 
+                 "rpow", "lshift", "rlshift", "rshift", "rrshift", "and", 
+                 "rand", "or", "ror", "xor", "rxor", "bool", "len", "contains", 
+                 "iter", "getitem", "setitem", "getattr", "setattr", "call", 
+                 "enter", "exit", "copy", "deepcopy"]
+     
     def __new__(cls, name, bases, dct):
         for op in Operable.OPERATORS:
             def op_func(*args, **kwargs):
@@ -25,8 +35,19 @@ class Operable(type):
     
 class Operation(metaclass=Operable):
     """
-    A class encapsulating operations to be performed instances of :class:`Operable` classes. :class:`Operation` objects can also respond to operators in the same manner that produces them (since :class:`Operable` is its metaclass), thereby creating nested :class:`Operation` objects. It is left to the eventual receiver of the :class:`Operation` object to decide whether this is acceptable.
-    :param op: An object representing the operation being performed on the provided arguments. This can be any object, since it is up to the eventual receiver of these objects to interpret this field. For example, the eventual receiver may want this to be a callable object that it can directly call on the arguments, or it may want it to be a string that it can interpret, etc.
+    A class encapsulating operations to be performed on instances of 
+    :class:`Operable` classes. :class:`Operation` objects can also respond to 
+    operators in the same manner that produces them (since :class:`Operable` 
+    is its metaclass), thereby creating nested :class:`Operation` objects. 
+    It is left to the eventual receiver of the :class:`Operation` object to 
+    decide whether this is acceptable.
+    
+    :param op: An object representing the operation being performed on the 
+    provided arguments. This can be any object, since it is up to the eventual 
+    receiver of these objects to interpret this field. For example, the eventual 
+    receiver may want this to be a callable object that it can directly call 
+    on the arguments, or it may want it to be a string that it can interpret, etc.
+    
     :type op: object
     """
     def __init__(self, op, *args, **kwargs):          
@@ -39,8 +60,19 @@ class Operation(metaclass=Operable):
     
 class Symbol(metaclass=Operable):
     """
-    A symbolic variable with a value not necessarily known to the user but guaranteed to be available at the time of translation. A canonical example of an :class:`Operable` class, it allows objects or values to be distributed throughout a program that depend on the future decisions of higher-level entities (e.g., a program translator deciding memory locations or array lengths). This requires operator evaluation to be deferred to a future time (and potentially in different ways, depending on the involved objects). In such a situation, this object acts as a placeholder for the desired object.
-    :param value: The value of the :class:`Symbol`, if known at instantiation. If not provided, may be assigned later.
+    A symbolic variable with a value not necessarily known to the user but 
+    guaranteed to be available at the time of translation. A canonical example 
+    of an :class:`Operable` class, it allows objects or values to be 
+    distributed throughout a program that depend on the future decisions of 
+    higher-level entities (e.g., a program translator deciding memory locations
+    or array lengths). This requires operator evaluation to be deferred to a 
+    future time (and potentially in different ways, depending on the involved 
+    objects). In such a situation, this object acts as a placeholder for the 
+    desired object.
+    
+    :param value: The value of the :class:`Symbol`, if known at instantiation. 
+    If not provided, may be assigned later.
+    
     :type value: object, optional
     """
     def __init__(self, value=None):
@@ -49,9 +81,15 @@ class Symbol(metaclass=Operable):
                 
     def assign(self, v, force=False):
         """
-        Assigns a value to the :class:`Symbol`. By default, assignment is only supposed to occur once, so an error will be thrown if this is called on an already-assigned :class:`Symbol`. This can be overridden by setting `force=True`.
+        Assigns a value to the :class:`Symbol`. By default, assignment is only
+        supposed to occur once, so an error will be thrown if this is called on
+        an already-assigned :class:`Symbol`. This can be overridden by setting 
+        `force=True`.
+        
         :param v: Value to assign
-        :param force: IF `True`, allows reassignment of already-assigned :class:`Symbol` objects
+        
+        :param force: IF `True`, allows reassignment of already-assigned instances.
+        
         :type force: bool
         """
         if Symbol.assigned(self) and not force:
@@ -75,9 +113,13 @@ class Symbol(metaclass=Operable):
     @staticmethod
     def assigned(obj):
         """
-        Analyzes the provided object to determine whether all encapsulated :class:`Symbol` objects are assigned. It evaluates :class:`Operation` objects by recursively checking on their arguments, and it evaluates :class:`Symbol` objects by checking whether they are assigned, and if so, whether their value is solvable. All other objects are assumed to be solvable.
-        :param obj: Object to check for solvability.
-        :return: `True` if the provided object has all 
+        Analyzes the provided object to determine whether all encapsulated 
+        :class:`Symbol` objects are assigned. It evaluates :class:`Operation` 
+        objects by recursively checking on their arguments, and it evaluates 
+        :class:`Symbol` objects by checking whether they are assigned. All 
+        other objects are assumed to be assigned by default.
+        
+        :param obj: Object to check for assignment.
         """
         if isinstance(obj, Operation):
             return reduce(and_, map(Symbol.assigned, obj._args + [v for k,v in obj._kwargs.items()]))
@@ -87,20 +129,57 @@ class Symbol(metaclass=Operable):
         
 class ManagedResource(Operable):
     """
-    Metaclass for creating autonomous hardware resource factories. This metaclass implements resource allocation by assigning unique IDs to created instances of derived classes (henceforth referred to as "resource objects"), as well as resource recycling by tracking whether the resource object is released in the future. Its implementation as a metaclass allows resource tracking to take place in a class field, which in turn allows multiple identical resource factories to be created and have them each track their own resources without interference simply by subclassing :class:`ManagedResource`. This also allows the resource objects themselves to be of any type. Additionally, it allows allocation to occur immediately when resource objects are created and potentially return existing resource objects if necessary. This is a subclass of :class:`Operable`, so that actions on resource objects can be inferred by analyzing produced :class:`Operation` objects.
-    :param instance_limit: The maximum number of instances that may be created by the class.
+    Metaclass for creating autonomous hardware resource factories. This 
+    metaclass implements resource allocation by assigning unique IDs to
+    created instances of derived classes (henceforth referred to as "resource 
+    objects"), as well as resource recycling by tracking whether the resource 
+    object is released in the future. Its implementation as a metaclass allows
+    resource tracking to take place in a class field, which in turn allows 
+    multiple identical resource factories to be created and have them each 
+    track their own resources without interference simply by subclassing 
+    :class:`ManagedResource`. This also allows the resource objects themselves 
+    to be of any type. Additionally, it allows allocation to occur immediately 
+    when resource objects are created and potentially return existing resource 
+    objects if necessary. This is a subclass of :class:`Operable`, so that 
+    actions on resource objects can be inferred by analyzing produced 
+    :class:`Operation` objects.
+    
+    :param instance_limit: The maximum number of instances that may be 
+    created by the class.
+    
     :type instance_limit: int, optional
-    :param use_instance_size: If `True`, indicates that the allocation offset should be increased by an amount equal to the provided `size` keyword.  
+    
+    :param use_instance_size: If `True`, indicates that the allocation 
+    offset should be increased by an amount equal to the provided `size` keyword.  
+    
     :type use_instance_size: bool, optional
     """
-    def __new__(cls_meta_new, name, bases, dct, instance_limit=None, use_instance_size=True):
+    def __new__(
+        cls_meta_new, 
+        name, bases, dct, 
+        instance_limit=None, 
+        use_instance_size=True):
         
-        def cls_new(cls, allocate=True, resource_id=None, *inst_args, **inst_kwargs):
+        def cls_new(cls, 
+                    allocate=True, 
+                    resource_id=None, *inst_args, **inst_kwargs):
             """
-            Create a new instance representing a hardware resource. If a resource limit has been provided and reached, existing allocations will be checked to see if they were released, and if so, may be returned. Created resources can also defer allocation until the future, allowing strategic control of the allocation value. 
-            :param allocate: If `False`, the returned instance will not be added to the list of managed resources, the :field:`resource_id` field will not be assigned, and the class :field:`allocation_index` field will not be updated.
+            Create a new instance representing a hardware resource. If a 
+            resource limit has been provided and reached, existing 
+            allocations will be checked to see if they were released, and if so,
+            may be returned. Created resources can also defer allocation until 
+            the future, allowing strategic control of the allocation value. 
+            
+            :param allocate: If `False`, the returned instance will not be 
+            added to the list of managed resources, the :field:`resource_id` 
+            field will not be assigned, and the class :field:`allocation_index` 
+            field will not be updated.
+            
             :type allocate: bool
-            :param resource_id: The ID of resource, which may be any type and whose interpretation is left to the owning class. If not provided, a new :class:`Symbol` will be instantiated.
+            
+            :param resource_id: The ID of resource, which may be any type and 
+            whose interpretation is left to the owning class. If not provided,
+            a new :class:`Symbol` will be instantiated.
             """
             if instance_limit is not None and len(cls.instances) >= instance_limit:
                 # Find a free instance we can use, as indicated by noting that it is released
@@ -123,8 +202,15 @@ class ManagedResource(Operable):
                         
         def allocate(self, force=False):
             """
-            Allocate the resource by assigning :field:`_resource_id`. If :field:`_resource_id` is a :class:`Symbol`, it is assigned while passing the keyword argument `force`. Otherwise, :class:`TypeError` is thrown, as resource IDs must be instances of :class:`Symbol` to be allocated.
-            :param force: The `force` argument passed to the :class:`Symbol` assignment.
+            Allocate the resource by assigning :field:`_resource_id`. If 
+            :field:`_resource_id` is a :class:`Symbol`, it is assigned while
+            passing the keyword argument `force`. Otherwise, :class:`TypeError`
+            is thrown, as resource IDs must be instances of :class:`Symbol` to
+            be allocated.
+            
+            :param force: The `force` argument passed to the :class:`Symbol` 
+            assignment.
+            
             :type force: `bool`
             """
             if isinstance(self._resource_id, Symbol):
@@ -137,8 +223,13 @@ class ManagedResource(Operable):
         
         def insert(cls, res, before=None, reallocate=True):
             """
-            Insert a resource before a specified resource in the instance list. If no resource is specified to determine the insertion location, the resource to be inserted is removed from its current location and appended to the end.
+            Insert a resource before a specified resource in the instance list.
+            If no resource is specified to determine the insertion location, 
+            the resource to be inserted is removed from its current location 
+            and appended to the end.
+            
             :param res: Resource to insert
+            
             :param before: Resource before which to insert `res`
             """
             cls.instances.remove(res)
@@ -150,17 +241,26 @@ class ManagedResource(Operable):
             
         def is_released(self):
             """
-            :return: `True` if the resource is currently released and available for use or reuse.
+            :return: `True` if the resource is currently released and available
+            for use or reuse.
+            
             :rtype: `bool`
             """
             return self._released
 
         def release(self):
             """
-            Release a resource, allowing it to be reused if the allocation limit of the class is reached.
+            Release a resource, allowing it to be reused if the allocation 
+            limit of the class is reached.
             """
             self._released = True
             
-        cls_attrs = {"instances": [], "allocation_index": 0, "__new__": cls_new, "allocate": allocate, "insert": classmethod(insert), "is_released": is_released, "release": release, **dct}
+        cls_attrs = {"instances": [], 
+                     "allocation_index": 0, 
+                     "__new__": cls_new, 
+                     "allocate": allocate, 
+                     "insert": classmethod(insert), 
+                     "is_released": is_released, 
+                     "release": release, **dct}
         return super().__new__(cls_meta_new, name, bases, cls_attrs)
         
