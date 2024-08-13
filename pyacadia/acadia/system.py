Fixed broadcasting issue in complex_to_sample, updated typical runtime structure to make running Runtimes from an interpreter in a terminal easier

pyacadia/acadia/system.py
@@ -1527,11 +1527,10 @@ class Acadia:
     
     def create_waveform(self,
                         channel: Channel,
-                        length: Union[int, float, np.ndarray],
-                        fixed: bool = False,
+                        length: Union[int, float, np.ndarray] = 0.0,
+                        fixed_length: Union[float, Symbol, Operation] = 0.0,
                         blank: bool = False,
                         decimation: Union[int, None] = 1,
-                        flat_top_length: Union[float, None] = None,
                         region: Union[Channel, ManagedMemory, None, str] = None) -> ChannelWaveform:
         """
         Allocate a waveform. This function allows for a few different signatures; 
@@ -1549,22 +1548,21 @@ class Acadia:
         - If a numpy array with an integer dtype, the argument is assumed to
             contain the sample data that will eventually be stored in the 
             waveform. The last dimension of the array must be of length 2.
+            Note that this does not store the sample data in any way; it just
+            uses it to determine the shape of aray must be allocated.
             
         :param channel: Channel for the waveform
         :type channel: :class:`Channel`
         :param length: The length of the waveform; see description above
         :type length: float, np.ndarray[complex], np.ndarray[float]
-        :param fixed: Create a fixed waveform. If ``True``, ``region`` must
-            be ``None``
-        :type fixed: bool
+        :param fixed_length: The length of the fixed part of the waveform, if any.
+        :type fixed_length: float, Symbol, or Operation
         :param blank: Create a blank waveform. If ``True``, ``region`` must
             be ``None``
         :type blank: bool
         :param decimation: Decimation for ADC waveforms
         :type decimation: int
-        :param flat_top_length: If nonzero, the length of time in seconds of
-            a constant region to add in the middle of the pulse
-        :type flat_top_length: float
+        :param region: The memory region in which the Waveform is stored
         """
         #################################################################
         # Start by validating the provided parameters
@@ -1584,13 +1582,9 @@ class Acadia:
         if channel.is_dac and decimation != 1:
             raise ValueError(f"Decimation must be 1 for DAC channels.")
         
-        if fixed or blank:
-            if region is not None:
-                raise TypeError(f"Cannot specify region for fixed or blank waveforms")
-            if flat_top_length is not None:
-                raise ValueError(f"Cannot have a flat top length for fixed or blank waveforms")
-            if decimation != 1:
-                raise ValueError(f"Cannot have decimation for fixed or blank waveforms")
+        if decimation != 1 and decimation % 4 != 0:
+            raise ValueError(f"Decimation may only be 1 or a multiple of 4"
+                             f" (received {decimation})")
 
         # Figure out how many samples will be processed at the channel interface
         # All channels use 32 bits per sample at the FIFO, so we can infer this directly from the tile config        
@@ -1599,31 +1593,28 @@ class Acadia:
                 region = channel.memory_type
                 channel_samples_per_cycle = self._firmware["rfdc"]["dac"]["channel_interface_width"][channel.num] // 32
             else:
-                if not fixed:
-                    raise TypeError(f"Allocating a waveform for ADC{channel.num}"
+                raise TypeError(f"Allocating a waveform for ADC{channel.num}"
                                     f" requires specifying a memory region.")
-                channel_samples_per_cycle = self._firmware["rfdc"]["adc"]["channel_interface_width"][channel.num] // 32
         else:
             # The source is either an ADC channel or a location in memory
             # ADC channels must have interface widths equal to the path width
             channel_samples_per_cycle = self._firmware["stream_processing_path"]["width"] // 32
 
-        if flat_top_length is not None:
-            if isinstance(flat_top_length, float) and flat_top_length > 0:
-                flat_top_length_cycles_float = flat_top_length * self._firmware["clocks"]["generated_clocks"]["seq_clk"]
-                flat_top_length_cycles = round(flat_top_length_cycles_float)
-                if flat_top_length_cycles != round(flat_top_length_cycles_float, 1):
-                    raise ValueError(f"Flat top length {flat_top_length} seconds"
-                                        f" is not an integer number of cycles"
-                                        f" ({flat_top_length_cycles_float} cycles)")
-            elif isinstance(flat_top_length, (Symbol, Operation)):
-                logger.warning(f"Unable to validate symbolic flat top length")
-                flat_top_length_cycles_float = flat_top_length * self._firmware["clocks"]["generated_clocks"]["seq_clk"]
-                flat_top_length_cycles = Operation(round, flat_top_length_cycles_float)
-            else:
-                raise TypeError(f"Unable to use flat top length {flat_top_length}")
+        if isinstance(fixed_length, float) and fixed_length >= 0:
+            fixed_length_cycles_float = fixed_length * self._firmware["clocks"]["generated_clocks"]["seq_clk"]
+            fixed_length_cycles = round(fixed_length_cycles_float)
+            if fixed_length_cycles != round(fixed_length_cycles_float, 1):
+                raise ValueError(f"Fixed length {fixed_length} seconds"
+                                    f" is not an integer number of cycles"
+                                    f" ({fixed_length_cycles_float} cycles)")
+        elif isinstance(fixed_length, (Symbol, Operation)):
+            logger.warning(f"Unable to validate symbolic fixed length;"
+                           " please ensure that the value provided is an"
+                           " integer number of cycles after being rounded.")
+            fixed_length_cycles_float = fixed_length * self._firmware["clocks"]["generated_clocks"]["seq_clk"]
+            fixed_length_cycles = Operation(round, fixed_length_cycles_float)
         else:
-            flat_top_length_cycles = None
+            raise TypeError(f"Unable to use fixed length {fixed_length}")
 
         #######################################################################
         # Given a length, determine the number of samples in the array
@@ -1651,7 +1642,20 @@ class Acadia:
             
             length_samples = input_length_samples // decimation
 
+            if length_cycles == 0:
+                if fixed_length_cycles == 0:
+                    raise ValueError("Waveform has total length zero.")
+                
+                logger.debug(f"Allocating FixedWaveform for channel {channel} with"
+                             f" a length of {fixed_length_cycles} cycles"
+                             f" ({channel_samples_per_cycle} samples per cycle)")
+                
+                return FixedChannelWaveform(channel, fixed_length_cycles, blank)
+                
             if decimation != 1:
+                if fixed_length_cycles != 0:
+                    raise ValueError("Fixed length must be zero for decimated waveforms.")
+                
                 logger.debug(f"Allocating DecimatedChannelWaveform for channel {channel} with"
                           f" {length_samples} samples, which corresponds to"
                           f" {length_cycles} cycles ({channel_samples_per_cycle}"
@@ -1659,20 +1663,18 @@ class Acadia:
                 
                 return DecimatedChannelWaveform(channel, length_samples, decimation, region)
             
-            if fixed:
-                logger.debug(f"Allocating FixedWaveform for channel {channel} of length"
-                             f" {length_cycles} cycles")
+            if fixed_length_cycles != 0:
+                # we have non-zero length and fixed length
+                if region is not None and not (channel.is_dac and region is channel.memory_type):
+                    raise ValueError(f"Region may not be specified for WindowedConstantWaveform objects.")
                 
-                return FixedChannelWaveform(channel, length_cycles, blank)
-            
-            if flat_top_length is not None:
                 logger.debug(f"Allocating WindowedConstantWaveform for channel {channel} with"
-                             f" a flat top length of {flat_top_length_cycles} cycles and"
+                             f" a fixed length of {fixed_length_cycles} cycles and"
                             f" {length_samples} window samples, which corresponds to"
                             f" {length_cycles} cycles ({channel_samples_per_cycle}"
                             f" samples per cycle)")
                 
-                return WindowedConstantWaveform(channel, length_samples, flat_top_length_cycles)
+                return WindowedConstantWaveform(channel, length_samples, fixed_length_cycles)
 
             logger.debug(f"Allocating ChannelWaveform for channel {channel} with"
                             f" {length_samples} samples, which corresponds to"
@@ -1680,20 +1682,6 @@ class Acadia:
                             f" samples per cycle)")
             
             return ChannelWaveform(channel, length_samples, resource_allocator=region)
-
-        elif isinstance(length, (Symbol, Operation)):
-            if not fixed:
-                raise ValueError("Symbolic length values may only be provided"
-                                 " for fixed waveforms.")
-            
-            logger.warning(f"Unable to validate symbolic length for fixed waveform")
-            length_cycles_float = length * self._firmware["clocks"]["generated_clocks"]["seq_clk"]
-            length_cycles = Operation(round, length_cycles_float)
-
-            logger.debug(f"Allocating FixedWaveform for channel {channel} of length"
-                             f" {length_cycles} cycles")
-
-            return FixedChannelWaveform(channel, length_cycles, blank)
 
         elif isinstance(length, np.ndarray):
             # When decimation is not 1, we don't have enough information to determine
@@ -1733,14 +1721,14 @@ class Acadia:
                 
                 return FixedChannelWaveform(channel, length_cycles, blank)
             
-            if flat_top_length is not None:
+            if fixed_length is not None:
                 logger.debug(f"Allocating WindowedConstantWaveform for channel {channel} with"
-                             f" a flat top length of {flat_top_length_cycles} cycles and"
+                             f" a flat top length of {fixed_length_cycles} cycles and"
                             f" {length_samples} window samples, which corresponds to"
                             f" {length_cycles} cycles ({channel_samples_per_cycle}"
                             f" samples per cycle)")
                 
-                return WindowedConstantWaveform(channel, length_samples, flat_top_length_cycles)
+                return WindowedConstantWaveform(channel, length_samples, fixed_length_cycles)
 
             logger.debug(f"Allocating ChannelWaveform for channel {channel} with"
                             f" {length_samples} samples, which corresponds to"
@@ -1943,7 +1931,7 @@ class Acadia:
     @requires_sequencer
     def configure_cmacc(self, 
                         src,
-                        kernel: Union[Waveform, float, None],
+                        kernel: Union[Waveform, float, None] = None,
                         write_mode: Union[str, None] = "upper", 
                         last_only: bool = True, 
                         reset_fifo: bool = False,
@@ -2024,7 +2012,7 @@ class Acadia:
                      f" 0x{control_reg:08X}")
         self.sequencer().bus_write(address=registers+2, data=control_reg)
 
-        return kernel
+        return configuration, kernel
         
     
     @requires_sequencer

pyacadia/acadia/waveforms.py
@@ -1,6 +1,7 @@
 from typing import Union
 from functools import reduce
 from operator import mul
+import logging
 
 import numpy as np
 from .channel import Channel
@@ -11,6 +12,8 @@ __all__ = ["Waveform",
            "DecimatedChannelWaveform",
            "FixedChannelWaveform", 
            "WindowedConstantWaveform"]
+
+logger = logging.getLogger("acadia")
 
 class Waveform:
     """
@@ -58,15 +61,13 @@ class Waveform:
     @property
     def __array_interface__(self) -> dict:
         if self._resource is None:
-            raise MemoryError(f"Attempted access of non-attached memory {self}"
-                              f" with resource {self._resource}")
+            raise MemoryError(f"Attempted access of unattached memory {self}")
         return self._resource.__array_interface__
     
     @property
     def byte_address(self) -> int:
         if self._resource is None:
-            raise MemoryError(f"Attempted access of non-attached memory {self}"
-                              f" with resource {self._resource}")
+            raise MemoryError(f"Attempted access of unattached memory {self}")
         return self._resource.byte_address
     
     @property
@@ -83,9 +84,8 @@ class Waveform:
     
     @property
     def array(self) -> np.ndarray:
-        if self._resource is None or self._resource.__array_interface__ is None:
-            raise MemoryError(f"Attempted access of non-attached memory {self}"
-                                f" with resource {self._resource}")
+        if self._resource is None:
+            raise MemoryError(f"Attempted access of unattached memory {self}")
         
         a = self._resource.__array_interface__
         return np.frombuffer(a["data"], 
@@ -158,10 +158,16 @@ class Waveform:
                 
                 # copyto will automatically broadcast if necessary
                 np.copyto(self.array, data)
+                return data
 
             elif data.dtype.kind == 'c':
                 # complex_to_sample will automatically take care of broadcasting a scalar
-                Waveform.complex_to_sample(data, output=self.array, scale=scale)
+                if self._resource is None:
+                    logger.warning(f"Attempted to set data of non-attached"
+                                   f" memory with array of shape {data.shape}.")
+                    return Waveform.complex_to_sample(data, scale=scale)
+                
+                return Waveform.complex_to_sample(data, output=self.array, scale=scale)
             else:
                 raise TypeError(f"Unable to convert waveform data of dtype"
                                 f" {data.dtype} to complex.")
@@ -174,7 +180,11 @@ class Waveform:
             
             if data[0] == "scipy":
                 from scipy.signal.windows import get_window
-                self.set(get_window(data[1], self.size), scale=scale)
+
+                logger.warning(f"Attempted to set data of non-attached"
+                                   f" memory with scipy window `{data[1]}`.")
+
+                return self.set(get_window(data[1], self.size), scale=scale)
             else:
                 raise ValueError(f"Unrecognized signature specifier for"
                                  f" waveform set: {data[0]}")
@@ -187,7 +197,7 @@ class Waveform:
                             output: Union[np.ndarray, np.dtype, None] = None, 
                             scale: Union[float, complex] = 1.0) -> np.ndarray:
         """
-        Convert sample data from its integer quadratures to complex 
+        Convert sample data from its signed integer quadratures to complex 
         floating-point numbers. Inputs must have an innermost dimension of 
         size 2.
         """
@@ -216,7 +226,7 @@ class Waveform:
         
         float_type = np.dtype(f"<f{output.dtype.itemsize // 2}")
 
-        scale *= 2**(input.dtype.itemsize*8 - 1) - 1 
+        scale *= 2**(input.dtype.itemsize*8 - 1) 
         np.multiply(np.reshape(input, -1), 
                     1/scale, 
                     out=np.reshape(output, -1).view(float_type), 
@@ -231,11 +241,16 @@ class Waveform:
                         scale: Union[float, complex] = 1.0) -> np.ndarray:
         """
         Convert complex floating-point data into integer samples and pack into
-        an array in the order expected by the RF tiles.
+        an array in the order expected by the RF tiles. The input 
+        floating-point values must be in the range [-1, 1). Note that the upper
+        bound is exclusive; the last valid value in the range is 1 - 2^-13.
         """
         if not hasattr(input, "dtype"):
             raise TypeError(f"Input must have a dtype (input is of type"
                             f" {type(input)})")
+        
+        if input.dtype.kind == "f":
+            input = input.astype(f"<c{input.dtype.itemsize*2}")
         
         if input.dtype.kind != "c":
             raise TypeError(f"Input dtype must be complex (found kind"
@@ -275,7 +290,7 @@ class Waveform:
 
         # left shift for packing 14-bit samples into 16-bit words
         rounded <<= 2
-        np.copyto(np.reshape(output, -1), rounded)
+        np.copyto(np.reshape(output, (-1, 2)), rounded)
         return output
         
 class ChannelWaveform(Waveform):

pyacadia/runtimes/loopback.py
@@ -11,6 +11,7 @@ class LoopbackRuntime(Runtime):
     stimulus: dict
     capture: dict
     iterations: int
+    plot: bool = True
         
     def main(self):     
         from acadia.system import Acadia
@@ -59,26 +60,29 @@ class LoopbackRuntime(Runtime):
 
     def initialize(self):
         # Set the matplotlib backend to one which we can actually update
-        from IPython.core.getipython import get_ipython
-        get_ipython().run_line_magic("matplotlib", "widget")
+        if self.plot:
+            from IPython.core.getipython import get_ipython
+            get_ipython().run_line_magic("matplotlib", "widget")
 
-        from acadia.processing import DynamicLine
-        import matplotlib.pyplot as plt
+            from acadia.processing import DynamicLine
+            import matplotlib.pyplot as plt
+
+            self.fig,self.ax = plt.subplots(1,1, figsize=(3,3))
+            self.fig.tight_layout()
+
+            self.line_re = DynamicLine(self.ax, ".-")
+            self.line_im = DynamicLine(self.ax, ".-")
+            self.ax.set_xlabel("Time [s]")
+            self.ax.set_ylabel("Signal Amplitude [arb. V]")
+            self.ax.grid()
+
+            self.time_axis = None
+
         from tqdm.notebook import tqdm
-
-        self.fig,self.ax = plt.subplots(1,1, figsize=(3,3))
-        self.fig.tight_layout()
-
-        self.line_re = DynamicLine(self.ax, ".-")
-        self.line_im = DynamicLine(self.ax, ".-")
-        self.ax.set_xlabel("Time [s]")
-        self.ax.set_ylabel("Signal Amplitude [arb. V]")
-        self.ax.grid()
-
         self.progress_bar = tqdm(desc="Iterations", dynamic_ncols=True, total=self.iterations)
         self.previous_completed_iterations = 0
 
-        self.time_axis = None
+        
         # self.fig.show()
 
     def update(self):
@@ -106,19 +110,19 @@ class LoopbackRuntime(Runtime):
         # Because the record group is uniform, when we get the records from the
         # group, they are stacked into a single array of shape (iterations, samples, 2)
         self.trace_summed = np.sum(self.data["traces"].records(), axis=0)
-        self.line_re.update(self.time_axis, self.trace_summed[:,0])
-        self.line_im.update(self.time_axis, self.trace_summed[:,1])
-        self.ax.relim()
-        self.ax.autoscale(tight=True)
-        self.fig.canvas.draw_idle()
+        
+        if self.plot:
+            self.line_re.update(self.time_axis, self.trace_summed[:,0])
+            self.line_im.update(self.time_axis, self.trace_summed[:,1])
+            self.ax.relim()
+            self.ax.autoscale(tight=True)
+            self.fig.canvas.draw_idle()
 
     def finalize(self):
         super().finalize()
         self.progress_bar.close()
 
-
-if __name__ == "__main__":    
-
+def run(plot=True):   
     stimulus: dict = {
         "channel": "DAC1",
 
@@ -153,12 +157,17 @@ if __name__ == "__main__":
 
         "waveform": {
             "length": 4.096e-6,
-            "decimation": 0,
+            "decimation": 4,
             "region": "plddr"
         }
     }
 
-    rt = LoopbackRuntime(stimulus, capture, iterations=100000)
+    rt = LoopbackRuntime(stimulus, capture, plot=plot, iterations=100000)
     rt.deploy("192.168.2.69", "loopback", files=[__file__], log_debug=True, remove_remote_directory=False, remote_directory="/home/root/%y%m%d_%H%M%S")    
     rt.display()
+
+    return rt
+
+if __name__ == "__main__":
+    rt = run()
     

pyacadia/runtimes/spectroscopy.py
@@ -12,11 +12,13 @@ class SpectroscopyRuntime(Runtime):
     
     stimulus: dict
     capture: dict
+
+    plot: bool = False
     
     # If ``0``, automatically fit phase data to 
     # extract an electrical delay. If any ``float``, this will be 
     # interpreted as the electrical delay to apply.
-    plot_electrical_delay: float = 0 
+    electrical_delay: float = 0 
 
     # The number of full spectra to take
     iterations: int = 10
@@ -71,43 +73,52 @@ class SpectroscopyRuntime(Runtime):
 
     def initialize(self):
         # Set the matplotlib backend to one which we can actually update
-        from IPython.core.getipython import get_ipython
-        get_ipython().run_line_magic("matplotlib", "widget")
+        if self.plot:
+            from IPython.core.getipython import get_ipython
+            get_ipython().run_line_magic("matplotlib", "widget")
 
-        from acadia.processing import DynamicLine
-        import matplotlib.pyplot as plt
-        from IPython.display import display
-        from ipywidgets import Label
-        from tqdm.notebook import tqdm
+            from acadia.processing import DynamicLine
+            import matplotlib.pyplot as plt
+            from IPython.display import display
+            from ipywidgets import Label
 
-        self.fig,ax = plt.subplots(1,2, figsize=(7,3))
-        self.fig.subplots_adjust(hspace=0.35)
-        self.fig.tight_layout()
+            self.fig,ax = plt.subplots(1,2, figsize=(7,3))
+            self.fig.subplots_adjust(hspace=0.35)
+            self.fig.tight_layout()
 
-        # Create a plot for the spectral magnitude
-        self.line_mag = DynamicLine(ax[0], ".-")
-        ax[0].set_xlabel("Frequency [MHz]")
-        ax[0].set_ylabel("Magnitude [arb. V*s]")
-        ax[0].set_title("Spectral Magnitude")
-        ax[0].grid()
-        ax[0].set_xlim(self.frequencies[0], self.frequencies[-1])
-        
-        # Create a plot for the spectral phase
-        self.line_phase = DynamicLine(ax[1], ".-")
-        ax[1].set_xlabel("Frequency [MHz]")
-        ax[1].set_ylabel("Phase [rad.]")
-        ax[1].set_title("Spectral Phase")
-        ax[1].grid()
-        ax[1].set_xlim(self.frequencies[0], self.frequencies[-1])
+            # Create a plot for the spectral magnitude
+            self.line_mag = DynamicLine(ax[0], ".-")
+            ax[0].set_xlabel("Frequency [MHz]")
+            ax[0].set_ylabel("Magnitude [arb. V*s]")
+            ax[0].set_title("Spectral Magnitude")
+            ax[0].grid()
+            ax[0].set_xlim(self.frequencies[0], self.frequencies[-1])
+            
+            # Create a plot for the spectral phase
+            self.line_phase = DynamicLine(ax[1], ".-")
+            ax[1].set_xlabel("Frequency [MHz]")
+            ax[1].set_ylabel("Phase [rad.]")
+            ax[1].set_title("Spectral Phase")
+            ax[1].grid()
+            ax[1].set_xlim(self.frequencies[0], self.frequencies[-1])
 
-        # Create a label for displaying the electrical delay
-        self._delay_label = Label("Electrical delay: ")
-        display(self._delay_label)
+            # Create a label for displaying the electrical delay
+            self._delay_label = Label("Electrical delay: ")
+            display(self._delay_label)
+
+            from tqdm.notebook import tqdm
+
+        else:
+            from tqdm import tqdm
 
         self.iterations_progress_bar = tqdm(desc="Iterations", dynamic_ncols=True, total=self.iterations)
         self.iterations_previous = 0
-        self.frequencies_progress_bar = tqdm(desc="Frequencies", dynamic_ncols=True, total=len(self.frequencies))
+        self.frequencies_progress_bar = tqdm(desc="Frequency Sweep Points", dynamic_ncols=True, total=len(self.frequencies)*self.iterations)
         self.frequencies_previous = 0
+
+        import numpy as np
+        self.data_summed = None
+        self.electrical_delay_phases = np.exp(2*np.pi*1j*self.frequencies*self.electrical_delay)
 
     def update(self):
         import numpy as np
@@ -121,11 +132,9 @@ class SpectroscopyRuntime(Runtime):
         # Update the progress bar based on the number of iterations
         completed_iterations = len(self.data["traces"]) // len(self.frequencies)
         self.iterations_progress_bar.update(completed_iterations - self.iterations_previous)
-        self.iterations_previous = completed_iterations
 
-        completed_frequencies = len(self.data["traces"]) % len(self.frequencies)
+        completed_frequencies = len(self.data["traces"])
         self.frequencies_progress_bar.update(completed_frequencies - self.frequencies_previous)
-        self.frequencies_previous = completed_frequencies
 
         # Only continue processing data if we have at least one complete iteration
         if completed_iterations == 0:
@@ -137,49 +146,61 @@ class SpectroscopyRuntime(Runtime):
         # Get the collection of data and reshape it so that the axes index as: 
         # (iteration, frequency, sample time, sample quadrature)
         samples_per_trace = data.shape[-2]
-        self.data_reshaped = data.reshape(-1, len(self.frequencies), samples_per_trace, 2)
+        data_reshaped = data.reshape(-1, len(self.frequencies), samples_per_trace, 2)
         
-        # Sum all the samples in a given trace and sum all the 
-        # iterations together for SNR
-        # TODO: we could do this much more intelligently by only adding the new traces
-        # that have been received since the last update, but for simplicity we'll leave it as is
-        self.data_summed = np.sum(self.data_reshaped, axis=(0,2))
+        # Slice the data so that we have an array containing only the traces
+        # we didn't have the last time update() was called
+        self.new_data = data_reshaped[self.iterations_previous:, :, :, :]
+
+        # Sum the new data and then add it to the aggregated array of trace data
+        new_data_summed = np.sum(self.new_data, axis=(0,2), keepdims=False)
+        if self.data_summed is None:
+            self.data_summed = new_data_summed
+        else:
+            self.data_summed += new_data_summed
 
         # Convert the summed sample data to a complex number and choose 
-        # the scale so that we turn the sum into an average
-        # At the same time, we'll remove the length-of-1 dimensions
-        self.data_complex = np.squeeze(Waveform.sample_to_complex(self.data_summed))
+        # the scale so that we turn the sum into a mean
+        # Simultaneously, choose the scale so that the result is independent
+        # of amplitude
+        scale = (self.stimulus["signal"]["scale"] / completed_iterations)
+        self.data_complex = Waveform.sample_to_complex(self.data_summed, scale=scale)
 
         # Apply the electrical delay
-        self.data_complex *= np.exp(2*np.pi*1j*self.frequencies*self.plot_electrical_delay)
+        self.data_complex *= self.electrical_delay_phases
 
         # We now have a 1D array of the amplitudes as a function of frequency,
         # so we can do whatever processing we want
         mags = np.abs(self.data_complex)
         phases = np.unwrap(np.angle(self.data_complex))
-        self.line_mag.update(self.frequencies, mags)
-        self.line_phase.update(self.frequencies, phases)
 
         # Update the fit
         def model(freqs, delay, phi0):
             return 2*np.pi*freqs*delay + phi0
     
         popt,pcov = curve_fit(model, self.frequencies, phases)
-        self._delay_label.value = f"Electrical delay = {round(popt[0]*1e9,1)} ns +/- {round(pcov[0,0]*1e12)} ps"
+        self.fit_electrical_delay = popt[0]
+        self.fit_electrical_delay_error = pcov[0,0]
 
         # Update the plot itself
-        self.fig.canvas.draw_idle()
+        if self.plot:
+            self.line_mag.update(self.frequencies, mags)
+            self.line_phase.update(self.frequencies, phases)
+            self._delay_label.value = f"Fit electrical delay = {round(self.fit_electrical_delay*1e9,1)} ns +/- {round(self.fit_electrical_delay_error*1e12)} ps"
+            self.fig.canvas.draw_idle()
 
         # Save the data
         self.data.save(self.local_directory)
+
+        self.iterations_previous = completed_iterations
+        self.frequencies_previous = completed_frequencies
 
     def finalize(self):
         super().finalize()
         self.iterations_progress_bar.close()
         self.frequencies_progress_bar.close()
 
-
-if __name__ == "__main__":
+def run(plot=True):
     import numpy as np
 
     stimulus: dict = {
@@ -191,13 +212,13 @@ if __name__ == "__main__":
         },
 
         "waveform": {
-            "length": 1e-6,
-            "flat_top_length": 1e-6
+            "length": 128e-9,
+            "fixed_length": 100e-6
         },
         
         "signal": {
             "data": ("scipy", "hann"),
-            "scale": 0.01
+            "scale": 0.25
         }
     }
     
@@ -205,22 +226,30 @@ if __name__ == "__main__":
         "channel": "ADC1",
 
         "datapath": {
-            "nyquist_zone": 2
+            "nyquist_zone": 1
         },
 
         "waveform": {
-            "length": 4e-6,
+            "length": 100e-6,
             "decimation": 0,
             "region": "plddr"
         }
     }
+
+    frequencies = np.linspace(9.012e9, 9.018e9, 121)
     
     # Run the program on the target
-    rt = SpectroscopyRuntime(frequencies=np.linspace(4.55e9, 4.9e9, 101),
+    rt = SpectroscopyRuntime(frequencies=frequencies,
                             stimulus=stimulus,
                             capture=capture,
                             iterations=1000,
-                            plot_electrical_delay=60e-9)
-    rt.deploy("192.168.2.69", "spectroscopy", files=[__file__])    
+                            plot=plot,
+                            electrical_delay=60e-9)
+    rt.deploy("192.168.2.70", "spectroscopy", files=[__file__])    
     rt.display()
+
+    return rt
+
+if __name__ == "__main__":
+    rt = run()
     

pyacadia/runtimes/variable_amplitude_spectroscopy.py
@@ -12,7 +12,8 @@ class VariableAmplitudeSpectroscopyRuntime(Runtime):
     amplitudes: list
     stimulus: dict
     capture: dict
-    plot_electrical_delay: float = 0 
+    plot: bool = False
+    electrical_delay: float = 0 
     iterations: int = 10
         
     def main(self):        
@@ -65,50 +66,57 @@ class VariableAmplitudeSpectroscopyRuntime(Runtime):
                     self.data.serve()
 
     def initialize(self):
-        # Set the matplotlib backend to one which we can actually update
-        from IPython.core.getipython import get_ipython
-        get_ipython().run_line_magic("matplotlib", "widget")
+        if self.plot:
+            # Set the matplotlib backend to one which we can actually update
+            from IPython.core.getipython import get_ipython
+            get_ipython().run_line_magic("matplotlib", "widget")
 
-        from acadia.processing import DynamicLine
-        import matplotlib.pyplot as plt
-        import matplotlib.colors as colors
-        import matplotlib.cm as cm
-        from IPython.display import display
-        from ipywidgets import Label
-        from tqdm.notebook import tqdm
+            from acadia.processing import DynamicLine
+            import matplotlib.pyplot as plt
+            import matplotlib.colors as colors
+            import matplotlib.cm as cm
+            from IPython.display import display
+            from ipywidgets import Label
 
-        self.fig,ax = plt.subplots(1,2, figsize=(7,3))
-        self.fig.subplots_adjust(hspace=0.35)
-        self.fig.tight_layout()
+            self.fig,ax = plt.subplots(1,2, figsize=(7,3))
+            self.fig.subplots_adjust(hspace=0.35)
+            self.fig.tight_layout()
 
-        # Create a plot for the spectral magnitude
-        cmap = plt.get_cmap("Spectral")
-        norm = colors.LogNorm(self.amplitudes[0], self.amplitudes[-1])
-        sm = cm.ScalarMappable(norm, cmap)
-        self.lines_mag = [DynamicLine(ax[0], ".-", c=sm.to_rgba(a)) for a in self.amplitudes]
-        ax[0].set_xlabel("Frequency [MHz]")
-        ax[0].set_ylabel("Magnitude [arb. V*s]")
-        ax[0].set_title("Spectral Magnitude")
-        ax[0].grid()
-        
-        # Create a plot for the spectral phase
-        self.lines_phase = [DynamicLine(ax[1], ".-", c=sm.to_rgba(a)) for a in self.amplitudes]
-        ax[1].set_xlabel("Frequency [MHz]")
-        ax[1].set_ylabel("Phase [rad.]")
-        ax[1].set_title("Spectral Phase")
-        ax[1].grid()
+            # Create a plot for the spectral magnitude
+            cmap = plt.get_cmap("Spectral")
+            norm = colors.LogNorm(self.amplitudes[0], self.amplitudes[-1])
+            sm = cm.ScalarMappable(norm, cmap)
+            self.lines_mag = [DynamicLine(ax[0], ".-", c=sm.to_rgba(a)) for a in self.amplitudes]
+            ax[0].set_xlabel("Frequency [MHz]")
+            ax[0].set_ylabel("Magnitude [arb. V*s]")
+            ax[0].set_title("Spectral Magnitude")
+            ax[0].grid()
+            
+            # Create a plot for the spectral phase
+            self.lines_phase = [DynamicLine(ax[1], ".-", c=sm.to_rgba(a)) for a in self.amplitudes]
+            ax[1].set_xlabel("Frequency [MHz]")
+            ax[1].set_ylabel("Phase [rad.]")
+            ax[1].set_title("Spectral Phase")
+            ax[1].grid()
 
-        # Create a label for displaying the electrical delay
-        self._delay_label = Label("Electrical delay: ")
-        display(self._delay_label)
+            # Create a label for displaying the electrical delay
+            self._delay_label = Label("Electrical delay: ")
+            display(self._delay_label)
+            from tqdm.notebook import tqdm
 
+        else:
+            from tqdm import tqdm
         self.iterations_progress_bar = tqdm(desc="Iterations", dynamic_ncols=True, total=self.iterations)
         self.iterations_previous = 0
-        self.amplitudes_progress_bar = tqdm(desc="Amplitudes", dynamic_ncols=True, total=len(self.amplitudes))
+        self.amplitudes_progress_bar = tqdm(desc="Amplitude Sweep Points", dynamic_ncols=True, total=len(self.amplitudes)*self.iterations)
         self.amplitudes_previous = 0
-        self.frequencies_progress_bar = tqdm(desc="Frequencies", dynamic_ncols=True, total=len(self.frequencies))
+        self.frequencies_progress_bar = tqdm(desc="Frequency Sweep Points", dynamic_ncols=True, total=len(self.frequencies)*len(self.amplitudes)*self.iterations)
         self.frequencies_previous = 0
 
+        import numpy as np
+        self.data_summed = None
+        self.data_complex = np.empty((len(self.amplitudes), len(self.frequencies)), dtype=np.complex128)
+        self.electrical_delay_phases = np.exp(2*np.pi*1j*self.frequencies*self.electrical_delay)
 
     def update(self):
         import numpy as np
@@ -121,17 +129,12 @@ class VariableAmplitudeSpectroscopyRuntime(Runtime):
         # Update the progress bars
         completed_iterations = len(self.data["traces"]) // (len(self.frequencies)*len(self.amplitudes))
         self.iterations_progress_bar.update(completed_iterations - self.iterations_previous)
-        self.iterations_previous = completed_iterations
 
-        extra_traces = len(self.data["traces"]) % (len(self.frequencies)*len(self.amplitudes))
-
-        completed_amplitudes = extra_traces // len(self.frequencies)
+        completed_amplitudes = len(self.data["traces"]) // len(self.frequencies)
         self.amplitudes_progress_bar.update(completed_amplitudes - self.amplitudes_previous)
-        self.amplitudes_previous = completed_amplitudes
 
-        completed_frequencies = extra_traces % len(self.frequencies)
+        completed_frequencies = len(self.data["traces"])
         self.frequencies_progress_bar.update(completed_frequencies - self.frequencies_previous)
-        self.frequencies_previous = completed_frequencies
 
         # Only continue processing data if we have at least one complete iteration
         if completed_iterations == 0:
@@ -141,23 +144,39 @@ class VariableAmplitudeSpectroscopyRuntime(Runtime):
         data = self.data["traces"].records()[:valid_traces, ...]
 
         samples_per_trace = data.shape[-2]
-        self.data_reshaped = data.reshape(-1, len(self.amplitudes), len(self.frequencies), samples_per_trace, 2)
-        self.data_summed = np.sum(self.data_reshaped, axis=(0,3))
+        data_reshaped = data.reshape(-1, len(self.amplitudes), len(self.frequencies), samples_per_trace, 2)
+        new_data = data_reshaped[self.iterations_previous:, :, :, :, :]
 
-        # Apply the electrical delay and update lines
-        # Don't rescale the plot when updating the lines, we'll do it all at once when we have the 
+        # Sum the new data and then add it to the aggregated array of trace data
+        new_data_summed = np.sum(new_data, axis=(0,3), keepdims=False)
+        if self.data_summed is None:
+            self.data_summed = new_data_summed
+        else:
+            self.data_summed += new_data_summed
+        
         for idx,amp in enumerate(self.amplitudes):
-            spectrum = np.squeeze(Waveform.sample_to_complex(self.data_summed[idx, ...], scale=amp))
-            spectrum *= np.exp(2*np.pi*1j*self.frequencies*self.plot_electrical_delay)
-            self.lines_mag[idx].update(self.frequencies, np.abs(spectrum), rescale_axis=False)
-            self.lines_phase[idx].update(self.frequencies, np.unwrap(np.angle(spectrum)), rescale_axis=False)
+            scale = amp / completed_iterations
+            self.data_complex[idx, :] = Waveform.sample_to_complex(self.data_summed[idx, :], scale=scale)
 
-        # Rescale axes and redraw plot
-        self.lines_mag[0]._ax.relim()
-        self.lines_mag[0]._ax.autoscale(tight=True)
-        self.lines_phase[0]._ax.relim()
-        self.lines_phase[0]._ax.autoscale(tight=True)
-        self.fig.canvas.draw_idle()
+        # Apply the electrical delay
+        self.data_complex *= self.electrical_delay_phases
+
+        if self.plot:
+            for idx,amp in enumerate(self.amplitudes):
+                # Don't rescale the plot when updating the lines, we'll do it all at once when we have the full plot
+                self.lines_mag[idx].update(self.frequencies, np.abs(self.data_complex[idx,:]), rescale_axis=False)
+                self.lines_phase[idx].update(self.frequencies, np.angle(self.data_complex[idx,:]), rescale_axis=False)
+
+            # Rescale axes and redraw plot
+            self.lines_mag[0]._ax.relim()
+            self.lines_mag[0]._ax.autoscale(tight=True)
+            self.lines_phase[0]._ax.relim()
+            self.lines_phase[0]._ax.autoscale(tight=True)
+            self.fig.canvas.draw_idle()
+
+        self.iterations_previous = completed_iterations
+        self.amplitudes_previous = completed_amplitudes
+        self.frequencies_previous = completed_frequencies
 
     def finalize(self):
         super().finalize()
@@ -165,12 +184,11 @@ class VariableAmplitudeSpectroscopyRuntime(Runtime):
         self.amplitudes_progress_bar.close()
         self.frequencies_progress_bar.close()
 
-
-if __name__ == "__main__":
+def run(plot=True):
     import numpy as np
 
     stimulus: dict = {
-        "channel": "DAC1",
+        "channel": "DAC4",
 
         "datapath": {
             "vop": 12000,
@@ -178,40 +196,47 @@ if __name__ == "__main__":
         },
 
         "waveform": {
-            "length": 1e-6,
-            "flat_top_length": 1e-6
+            "length": 128e-9,
+            "flat_top_length": 100e-6
         },
         
         "signal": {
             "data": ("scipy", "hann"),
-            "scale": 1
+            "scale": 0.5
         }
     }
-    
+
     capture: dict = {
-        "channel": "ADC1",
+        "channel": "ADC4",
 
         "datapath": {
-            "nyquist_zone": 2
+            "nyquist_zone": 1
         },
 
         "waveform": {
-            "length": 4e-6,
+            "length": 100e-6,
             "decimation": 0,
             "region": "plddr"
         }
     }
-    
-    frequencies = np.linspace(4.55e9, 4.9e9, 101)
-    amplitudes = np.logspace(-2, 0, 21)
+
+    frequencies = np.linspace(9.01e9, 9.02e9, 101)
+    amplitudes = np.linspace(0.1, 1, 10)
 
     # Run the program on the target
-    rt = VariableAmplitudeSpectroscopyRuntime(frequencies=frequencies,
-                             amplitudes=amplitudes,
-                            stimulus=stimulus,
-                            capture=capture,
-                            iterations=1000,
-                            plot_electrical_delay=60e-9)
-    rt.deploy("192.168.2.69", "variable_amplitude_spectroscopy", files=[__file__])    
+    rt = VariableAmplitudeSpectroscopyRuntime(
+        frequencies=frequencies,
+        amplitudes=amplitudes,
+        stimulus=stimulus,
+        capture=capture,
+        iterations=1000,
+        plot=plot,
+        electrical_delay=108e-9)
+    rt.deploy("192.168.2.70", "variable_amplitude_spectroscopy", files=[__file__])    
     rt.display()
+    
+    return rt
+
+if __name__ == "__main__":
+    rt = run()
     