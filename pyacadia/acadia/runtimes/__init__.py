470a81cf8733b7c8536a6d04f907a1ebecdd131d
CHAO ZHOU <chao.zhou.cz445@yale.edu>
2025-02-28 15:43:50 -0500

    removed unavailable runtimes from __init__.py

--- pyacadia/acadia/runtimes/__init__.py
+++ pyacadia/acadia/runtimes/__init__.py
@@ -1,15 +1,15 @@
 from .continuous_synthesis import ContinuousSynthesisRuntime
 from .dsp_spectroscopy import DSPSpectroscopyRuntime
 from .loopback import LoopbackRuntime
-from .qubit_spectroscopy import QubitSpectroscopyRuntime
+# from .qubit_spectroscopy import QubitSpectroscopyRuntime
 from .spectroscopy import SpectroscopyRuntime
-from .variable_amplitude_spectroscopy import VariableAmplitudeSpectroscopyRuntime
+# from .variable_amplitude_spectroscopy import VariableAmplitudeSpectroscopyRuntime
 from .delay_calibration import DelayCalibrationRuntime
 
 __all__ = ["ContinuousSynthesisRuntime",
            "DSPSpectroscopyRuntime",
            "LoopbackRuntime",
-           "QubitSpectroscopyRuntime",
+        #    "QubitSpectroscopyRuntime",
            "SpectroscopyRuntime",
-           "VariableAmplitudeSpectroscopyRuntime",
+        #    "VariableAmplitudeSpectroscopyRuntime",
            "DelayCalibrationRuntime"]
