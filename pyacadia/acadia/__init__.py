b390044c9020abdc2c4696d1a3f7b66ef9d60299
CHAO ZHOU <chao.zhou.cz445@yale.edu>
2025-04-11 19:50:26 -0400

    removed unavailable imports from __init__.py, fixed typo in `continuous_synthesis.py`

--- pyacadia/acadia/__init__.py
+++ pyacadia/acadia/__init__.py
@@ -2,7 +2,7 @@ from acadia.rfdc import Channel
 from .compiler import Operation, Symbol
 from .data import DataManager
 from .peripherals import RFClk, PSGPIO, ZDMA, AXISSwitch, ZCU216Sensors
-from .processing import DynamicLine, DynamicErrorbar, ProgressBar, DynamicReadoutHistogram
+from .processing import DynamicLine, DynamicErrorbar, ProgressBar
 from .runtime import Runtime
 from .system import Acadia
 from .waveforms import WaveformMemory, ChannelWaveformMemory, FixedChannelWaveformMemory, DecimatedChannelWaveformMemory, WindowedConstantWaveformMemory
@@ -14,7 +14,7 @@ __all__ = ["Channel",
             "Operation", "Symbol",
            "DataManager",
            "RFClk", "PSGPIO", "ZDMA", "AXISSwitch", "ZCU216Sensors",
-           "DynamicLine", "DynamicErrorbar", "ProgressBar", "DynamicReadoutHistogram",
+           "DynamicLine", "DynamicErrorbar", "ProgressBar",
            "Runtime",
            "Acadia",
            "WaveformMemory", "ChannelWaveformMemory", "FixedChannelWaveformMemory", "DecimatedChannelWaveformMemory", "WindowedConstantWaveformMemory"] + runtimes_all
