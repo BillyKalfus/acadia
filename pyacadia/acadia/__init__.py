25419388299139810a109e5a5247008664f956bf
Billy Kalfus <william.kalfus@yale.edu>
2025-04-22 18:12:16 -0400

    Fixed broken import in __init__

--- pyacadia/acadia/__init__.py
+++ pyacadia/acadia/__init__.py
@@ -2,11 +2,7 @@ from acadia.rfdc import Channel
 from .compiler import Operation, Symbol
 from .data import DataManager
 from .peripherals import RFClk, PSGPIO, ZDMA, AXISSwitch, ZCU216Sensors
-<<<<<<< HEAD
 from .processing import DynamicLine, DynamicErrorbar
-=======
-from .processing import DynamicLine, DynamicErrorbar, ProgressBar
->>>>>>> 071c7754ae1683c1fb91d5c9df9ef56762b027df
 from .runtime import Runtime
 from .system import Acadia
 from .waveforms import WaveformMemory, ChannelWaveformMemory, FixedChannelWaveformMemory, DecimatedChannelWaveformMemory, WindowedConstantWaveformMemory
@@ -18,11 +14,7 @@ __all__ = ["Channel",
             "Operation", "Symbol",
            "DataManager",
            "RFClk", "PSGPIO", "ZDMA", "AXISSwitch", "ZCU216Sensors",
-<<<<<<< HEAD
            "DynamicLine", "DynamicErrorbar",
-=======
-           "DynamicLine", "DynamicErrorbar", "ProgressBar",
->>>>>>> 071c7754ae1683c1fb91d5c9df9ef56762b027df
            "Runtime",
            "Acadia",
            "WaveformMemory", "ChannelWaveformMemory", "FixedChannelWaveformMemory", "DecimatedChannelWaveformMemory", "WindowedConstantWaveformMemory"] + runtimes_all
